eb32b63e1f7cf7f0a33f62a525caad23aad74fd9
Rogerio França de Abreu <ggustavofranca2006@gmail.com>
2025-02-11 19:06:23 -0300

    first commit

---
+++ .vscode/script.js
@@ -0,0 +1,13 @@
+function toggleModer() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatarr.png")
+    
+  } else {
+    img.setAttribute("src", "./assets/avatarpp.png")
+  }
+}
